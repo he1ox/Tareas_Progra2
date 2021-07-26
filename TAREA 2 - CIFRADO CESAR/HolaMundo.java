d708eed6e6ec3bcdeb9b157a99f91d3c46447760
Jorge Lopez <jlopezg112@miumg.edu.gt>
2021-07-25 21:39:43 -0600

    tarea 2 - codigo formateado

--- TAREA 2 - CIFRADO CESAR/HolaMundo.java
+++ TAREA 2 - CIFRADO CESAR/HolaMundo.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author georg
+ * @author Jorge López
  */
 //Importamos todas las clases dentro del paquete util
 import java.util.*;
@@ -14,40 +8,38 @@ import java.util.*;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        String palabra = Ascii("codificar","Jorge",4);
+        String palabra = Ascii("codificar", "Jorge", 4);
         System.out.println(palabra);
-       
+
         //Jorge = Nsvki, Clave 4;
-        
-        String palabra2 = Ascii("decodificar","Nsvki",4);
+        String palabra2 = Ascii("decodificar", "Nsvki", 4);
         System.out.println(palabra2);
-        
-        
+
         System.out.println("--FUERZA BRUTA---");
-        
-        String palabras = fuerzaBruta("Nsvki",0,10);
+
+        String palabras = fuerzaBruta("Nsvki", 0, 10);
         System.out.println(palabras);
     }
 
-
     /**
-    * Este método es capaz de codificar/decodificar String con el cifrado cesar.
-    * @param opcion "codificar", "decodificar".
-    * @param palabra String a operar con el algoritmo.
-    * @param clave valor número que alterá el orden lógico del String.
-    * @return String con encriptación/desencriptación.
-    */
-    
-    public static String Ascii(String opcion, String palabra, int clave){
+     * Este método es capaz de codificar/decodificar String con el cifrado
+     * cesar.
+     *
+     * @param opcion "codificar", "decodificar".
+     * @param palabra String a operar con el algoritmo.
+     * @param clave valor número que alterá el orden lógico del String.
+     * @return String con encriptación/desencriptación.
+     */
+    public static String Ascii(String opcion, String palabra, int clave) {
         String palabraDecodificada = "";
         char letraDecodificada;
         opcion = opcion.toLowerCase();
-        
-        for(int i = 0 ; i<palabra.length() ; i++){
-            if(palabra.charAt(i) != ' '){
-                letraDecodificada = (char)palabra.charAt(i);
-                
-                switch(opcion){
+
+        for (int i = 0; i < palabra.length(); i++) {
+            if (palabra.charAt(i) != ' ') {
+                letraDecodificada = (char) palabra.charAt(i);
+
+                switch (opcion) {
                     case "codificar":
                         letraDecodificada += clave;
                         break;
@@ -58,7 +50,7 @@ public class HolaMundo {
                         letraDecodificada += clave;
                         break;
                 }
-                
+
                 palabraDecodificada += letraDecodificada;
             } else {
                 palabraDecodificada += " ";
@@ -66,38 +58,36 @@ public class HolaMundo {
         }
         return palabraDecodificada;
     }
-    
-    
-     /**
-    * Fuerza bruta aplicada al cifrado cesar.
-    * @param inicio clave con que se comenzará a operar
-    * @param palabra String a operar con el algoritmo.
-    * @param fin clave final a operar
-    * @return String con encriptación/desencriptación.
-    */
-    public static String fuerzaBruta(String palabra, int inicio, int fin){
-        
+
+    /**
+     * Fuerza bruta aplicada al cifrado cesar.
+     *
+     * @param inicio clave con que se comenzará a operar
+     * @param palabra String a operar con el algoritmo.
+     * @param fin clave final a operar
+     * @return String con encriptación/desencriptación.
+     */
+    public static String fuerzaBruta(String palabra, int inicio, int fin) {
+
         String palabraDecodificada = "";
         char letraDecodificada;
-        
-        for(int j = inicio ; j<fin ; j++){
-            
-            for(int i = 0 ; i<palabra.length() ; i++){
-                
-                if(palabra.charAt(i) != ' '){
-                    letraDecodificada = (char)palabra.charAt(i);
+
+        for (int j = inicio; j < fin; j++) {
+
+            for (int i = 0; i < palabra.length(); i++) {
+
+                if (palabra.charAt(i) != ' ') {
+                    letraDecodificada = (char) palabra.charAt(i);
                     letraDecodificada -= j;
                     palabraDecodificada += letraDecodificada;
-                } 
-                else 
-                {
+                } else {
                     palabraDecodificada += " ";
                 }
-                
+
             }
-            palabraDecodificada += ", CLAVE: "+j+"\n";
+            palabraDecodificada += ", CLAVE: " + j + "\n";
         }
-        
+
         return palabraDecodificada;
     }
 
